4805e040c67cab3d5e54ca35a9c6cfe422bfe749
Yishai Hezi <yishai.hezi@moovit.com>
2024-07-28 16:40:03 +0300

    Add new theme colors

--- app/src/main/java/com/example/worlddata/HomeActivity.kt
+++ app/src/main/java/com/example/worlddata/HomeActivity.kt
@@ -3,7 +3,13 @@ package com.example.worlddata
 import androidx.appcompat.app.AppCompatActivity
 
 /**
+ * The main activity / screen of the application.
  *
  * @author Yishai Hezi
  */
-class HomeActivity : AppCompatActivity(R.layout.home_activity)
+class HomeActivity : AppCompatActivity(R.layout.home_activity) {
+
+
+
+}
+
