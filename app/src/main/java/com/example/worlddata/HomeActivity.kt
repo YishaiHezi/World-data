2f99e7883a3c445662a708505396225ec50457f9
Yishai Hezi <yishai.hezi@moovit.com>
2024-07-28 14:57:39 +0300

    New home activity

---
+++ app/src/main/java/com/example/worlddata/HomeActivity.kt
@@ -0,0 +1,9 @@
+package com.example.worlddata
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @author Yishai Hezi
+ */
+class HomeActivity : AppCompatActivity(R.layout.home_activity)
