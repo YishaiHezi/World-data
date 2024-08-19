5a17a79c6d43f42850811327677450c1ece1ab06
Yishai Hezi <yishai.hezi@moovit.com>
2024-08-19 16:22:01 +0300

    Add a divider between items

--- app/src/main/java/com/example/worlddata/CountryRatingFragment.kt
+++ app/src/main/java/com/example/worlddata/CountryRatingFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 
@@ -32,10 +34,14 @@ class CountryRatingFragment : Fragment(R.layout.country_rating_fragment) {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+
 		viewLifecycleOwner.lifecycleScope.launch {
 			repeatOnLifecycle(Lifecycle.State.STARTED) {
-				viewModel.countriesFlow.collect {
-					view.findViewById<RecyclerView>(R.id.recycler_view).adapter = CountriesListAdapter(it)
+				viewModel.countriesFlow.collect { countries ->
+					if (recyclerView.adapter == null || (recyclerView.adapter as CountriesListAdapter).countries != countries) {
+						initRecyclerView(recyclerView, countries)
+					}
 				}
 			}
 		}
@@ -43,9 +49,34 @@ class CountryRatingFragment : Fragment(R.layout.country_rating_fragment) {
 
 
 	/**
+	 * Initialize the recycler view.
+	 */
+	private fun initRecyclerView(recyclerView: RecyclerView, countries: List<CountryItem>) {
+		recyclerView.adapter = CountriesListAdapter(countries)
+		addDividers(recyclerView)
+	}
+
+
+	/**
+	 * Add dividers between elements to the recyclerView.
+	 */
+	private fun addDividers(recyclerView: RecyclerView?) {
+		val context = recyclerView?.context ?: return
+		val divider = ContextCompat.getDrawable(context, R.drawable.divider_horizontal)
+		val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL).apply {
+			if (divider != null) {
+				setDrawable(divider)
+			}
+		}
+
+		recyclerView.addItemDecoration(dividerItemDecoration)
+	}
+
+
+	/**
 	 * The adapter for the recycler view that shows all the countries.
 	 */
-	private class CountriesListAdapter(private val countries: List<CountryItem>) :
+	private class CountriesListAdapter(val countries: List<CountryItem>) :
 		RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 		override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 			val view = LayoutInflater.from(parent.context).inflate(R.layout.country_item, parent, false)
