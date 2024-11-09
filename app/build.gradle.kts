4fb92cc94f86ce6f7e5d9f062d66a1ad0dfb3404
Yishai Hezi <yishai.hezi@moovit.com>
2024-11-09 20:09:38 +0200

    Add a new library for json serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	alias(libs.plugins.jetbrains.kotlin.android)
 	id("kotlin-kapt")
 	id("com.google.dagger.hilt.android")
+	kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -51,6 +52,7 @@ dependencies {
 	implementation (libs.androidx.room.ktx)
 	implementation(libs.kotlinx.coroutines.core)
 	implementation(libs.kotlinx.coroutines.android)
+	implementation(libs.kotlinx.serialization.json)
 }
 
 kapt {
