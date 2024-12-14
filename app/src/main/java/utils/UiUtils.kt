fd0a0e7956a98907f896321d1425e8a38f101a57
Yishai Hezi <yishai.hezi@moovit.com>
2024-12-14 13:39:33 +0200

    Add a new method to UiUtils

--- app/src/main/java/utils/UiUtils.kt
+++ app/src/main/java/utils/UiUtils.kt
@@ -2,6 +2,18 @@ package utils
 
 import android.view.View
 
+/**
+ * Set the visibility of a set of views to the same value.
+ */
 fun setSameVisibility(source: View, dependent: View) {
     dependent.visibility = source.visibility
+}
+
+
+/**
+ * Update the visibility of a set of views based on a condition.
+ */
+fun updateVisibility(vararg views: View, condition: Boolean?) {
+    val visibility = if (condition == true) View.VISIBLE else View.GONE
+    views.forEach { it.visibility = visibility }
 }
