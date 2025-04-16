a4db233a4d092bad456e4acda4d37ec94b39b69c
Yishai Hezi <yishai.hezi@moovit.com>
2025-04-16 18:13:37 +0300

    Implement the view model for the quiz screen

---
+++ app/src/main/java/com/example/worlddata/QuizViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.worlddata
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+/**
+ * The view model needed for the quiz screen.
+ */
+class QuizViewModel : ViewModel() {
+
+
+    /**
+     * Mutable ui state for the quiz screen.
+     */
+    private val mutableUiState = MutableStateFlow(
+        UiState(
+            imageRes = R.drawable.ic_flag_fi,
+            question = "This is a test question! what flag is this?",
+            answers = listOf("Saudi Arabia", "Pakistan", "Turkey", "Afghanistan"),
+            correctAnswer = "Afghanistan"
+        )
+    )
+
+
+    /**
+     * Immutable ui state for the quiz screen.
+     */
+    val uiState: StateFlow<UiState> = mutableUiState
+
+
+}
+
+
+/**
+ * Holds the data needed for a single question in the quiz.
+ */
+data class UiState(
+    val imageRes: Int,
+    val question: String,
+    val answers: List<String>,
+    val correctAnswer: String
+
+)
